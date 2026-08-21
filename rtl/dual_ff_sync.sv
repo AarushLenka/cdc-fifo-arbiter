ea04b05cdc52b9788434eca96db6c19d9272fb45
Aarush Lenka <lenkaaarush@gmail.com>
2026-08-21 22:54:43 +0530

    [rtl] added synchronizer rtl

---
+++ rtl/dual_ff_sync.sv
@@ -0,0 +1,20 @@
+module dual_ff_sync (
+    input  logic clk,
+    input  logic rst_n,
+    input  logic d0,
+    output logic q
+);
+
+    logic q0;
+
+    always_ff @(posedge clk or negedge rst_n) begin : sync
+        if (!rst_n) begin
+            q0 <= 1'b0;
+            q  <= 1'b0;
+        end else begin
+            q0 <= d0;
+            q  <= q0;
+        end
+    end
+
+endmodule
