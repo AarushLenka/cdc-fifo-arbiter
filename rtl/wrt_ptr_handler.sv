f1a9ca2811eb93d254d933e3f8a6b7ceeeefdb88
Aarush Lenka <lenkaaarush@gmail.com>
2026-09-10 23:06:42 +0530

    [rtl fix:wrt_ptr_handler

--- rtl/wrt_ptr_handler.sv
+++ rtl/wrt_ptr_handler.sv
@@ -3,24 +3,24 @@ import fifo_pkg::*;
 module wrt_ptr_handler (
     input logic wclk,
     input logic wrst_n,
-    input logic w_en; 
+    input logic w_en,
     input logic [A_WIDTH:0] g_rptr_sync,
     output logic [A_WIDTH:0] g_wptr, 
     output logic [A_WIDTH:0] b_wptr,
-    output logic [A_WIDTH:0] full
+    output logic full
 );
     logic [A_WIDTH:0] b_wptr_next, g_wptr_next;
     logic wfull;
 
-    assign b_wptr_next = b_wptr + (~full && w_en);
+    assign b_wptr_next = b_wptr + (~wfull && w_en);
     assign g_wptr_next = (b_wptr_next>>1)^b_wptr_next;
     assign wfull = (g_wptr_next=={~g_rptr_sync[A_WIDTH:A_WIDTH-1],g_rptr_sync[A_WIDTH-2:0]});
 
-    always_ff @( posedge clk or or negedge wrst_n ) begin : registers
+    always_ff @( posedge wclk or negedge wrst_n ) begin : registers
         if (!wrst_n) begin
             b_wptr <= '0;
             g_wptr <= '0;
-            wfull <= 1'b0;
+            full <= 1'b0;
         end
         else begin
             b_wptr <= b_wptr_next;
